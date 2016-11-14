963cccacb83f4958a96c9b64c81e1d913be59976
Leonardo Calasans <calasans.leonardo@gmail.com>
2016-11-14 19:03:27 -0300

    Add 'putdown' action and 'del' clause

--- Blocks.pl
+++ Blocks.pl
@@ -6,6 +6,14 @@ member(X, [X|Tail]).
 member(X, [Head|Tail]) :-
 	member(X, Tail).
 
+%Deletes X from list
+
+del(X, [X|L1] , L1).
+del(X, [Head|L] , [Head|L1]) :-
+	member(X,L),
+	del(X,L,L1).
+
+
 
 
 
@@ -20,3 +28,6 @@ put_on_table(X) :-
 	member(held(X), Ambient),
 	remove&add(held(X) , on(X,m), Ambient).
 
+putdonw(X,Y) :-
+	member(held(X), Ambient),
+	remove&add(held(X), on(X,Y), Ambient).

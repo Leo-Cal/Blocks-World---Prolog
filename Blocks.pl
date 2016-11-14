1736e45722b0417f7b2e43d6cae1dcbf01c2c46f
Leonardo Calasans <calasans.leonardo@gmail.com>
2016-11-14 18:38:33 -0300

    Add Actions

---
+++ Blocks.pl
@@ -0,0 +1,13 @@
+
+
+%Actions
+
+pickup(X) :-
+	not(member(held(_), Ambient)),
+	not(member(on(_,X), Ambient)),
+	remove&add( on(X,_), held(X), Ambient).
+
+put_on_table(X) :-
+	member(held(X), Ambient),
+	remove&add(held(X) , on(X,m), Ambient).
+

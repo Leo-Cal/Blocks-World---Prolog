0d8eefb63c8f11f9f04207090cfadf2510eed5e4
Leonardo Calasans <calasans.leonardo@gmail.com>
2016-11-23 18:03:12 -0300

    Sorted actions by each block

--- Blocks.pl
+++ Blocks.pl
@@ -20,28 +20,79 @@ add(X,L, [X|L]).
 
 %Actions
 
-
-pickup(X) :-
+%Pickups
+action(Ambient,pickup_a,Ambient2) :-
 	not(member(held(_), Ambient)),
-	not(member(on(_,X), Ambient)),
-        del( on(X,_), Ambient, Ambient1),
-	add(held(X), Ambient1, Ambient2).
+	not(member(on(_,a), Ambient)),
+        del( on(a,_), Ambient, Ambient1),
+	add(held(a), Ambient1, Ambient2).
 
-put_on_table(X) :-
-	member(held(X), Ambient),
-        del(held(X), Ambient, Ambient1),
-	add(on(X,m), Ambient1, Ambient2).
+action(Ambient,pickup_b,Ambient2) :-
+	not(member(held(_), Ambient)),
+	not(member(on(_,b), Ambient)),
+        del( on(b,_), Ambient, Ambient1),
+	add(held(b), Ambient1, Ambient2).
 
-putdown(X,Y) :-
-	member(held(X), Ambient),
-        del(held(X), Ambient, Ambient1),
-	add(on(X,Y), Ambient1, Ambient2).
+action(Ambient,pickup_c,Ambient2) :-
+	not(member(held(_), Ambient)),
+	not(member(on(_,c), Ambient)),
+        del( on(c,_), Ambient, Ambient1),
+	add(held(c), Ambient1, Ambient2).
+
+%Puts on Table
+action(Ambient,put_a_on_table, Ambient2) :-
+	member(held(a), Ambient),
+        del(held(a), Ambient, Ambient1),
+	add(on(a,m), Ambient1, Ambient2).
+
+action(Ambient,put_b_on_table, Ambient2) :-
+	member(held(b), Ambient),
+        del(held(b), Ambient, Ambient1),
+	add(on(b,m), Ambient1, Ambient2).
+
+action(Ambient,put_c_on_table, Ambient2) :-
+	member(held(c), Ambient),
+        del(held(c), Ambient, Ambient1),
+	add(on(c,m), Ambient1, Ambient2).
+
+%Putdowns
+action(Ambient,putdown_ab,Ambient2) :-
+	member(held(a), Ambient),
+        del(held(a), Ambient, Ambient1),
+	add(on(a,b), Ambient1, Ambient2).
+
+action(Ambient,putdown_ac,Ambient2) :-
+	member(held(a), Ambient),
+        del(held(a), Ambient, Ambient1),
+	add(on(a,c), Ambient1, Ambient2).
+
+action(Ambient,putdown_ba,Ambient2) :-
+	member(held(b), Ambient),
+        del(held(b), Ambient, Ambient1),
+	add(on(b,a), Ambient1, Ambient2).
+
+action(Ambient,putdown_bc,Ambient2) :-
+	member(held(b), Ambient),
+        del(held(b), Ambient, Ambient1),
+	add(on(b,c), Ambient1, Ambient2).
+
+action(Ambient,putdown_ca,Ambient2) :-
+	member(held(c), Ambient),
+        del(held(c), Ambient, Ambient1),
+	add(on(c,a), Ambient1, Ambient2).
+
+action(Ambient,putdown_cb,Ambient2) :-
+	member(held(c), Ambient),
+        del(held(c), Ambient, Ambient1),
+	add(on(c,b), Ambient1, Ambient2).
+
+
 
 %Solving
 
 solve(Ambient1, Ambient1).
 solve(Ambient1,Ambient2) :-
-	move(Ambient1, Move, Intermediary),
+	action(Ambient1, Move, Intermediary),
 	solve(Intermediary, Ambient2).
 
 

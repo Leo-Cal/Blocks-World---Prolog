6b2ddd7b647f0018e55b1bbd4a031324c31c5f3d
Leonardo Calasans <calasans.leonardo@gmail.com>
2016-12-14 20:26:09 -0300

    Add precond to putdown

--- Blocks.pl
+++ Blocks.pl
@@ -1,98 +1,43 @@
+:-dynamic clear/1.
+:-dynamic on/2.
+:-dynamic held/1.
 
 
-%Defines if X is member of list L
 
-member(X, [X|Tail]).
-member(X, [Head|Tail]) :-
-	member(X, Tail).
+%Initial State
+clear(hand).
+on(a,table).
+on(b,table).
+on(c,table).
+clear(a).
+clear(b).
+clear(c).
+clear(table).
 
-%Deletes X from list
 
-del(X, [X|L1] , L1).
-del(X, [Head|L] , [Head|L1]) :-
-	member(X,L),
-	del(X,L,L1).
-
-%Adds X to head of list
-
-add(X,L, [X|L]).
 
 
 %Actions
 
-%Pickups
-action(Ambient,pickup_a,Ambient2) :-
-	not(member(held(_), Ambient)),
-	not(member(on(_,a), Ambient)),
-        del( on(a,_), Ambient, Ambient1),
-	add(held(a), Ambient1, Ambient2).
+pickup(X) :-
+      %precond
+	clear(hand),
+	clear(X),
+      %effects
+	assert(held(X)),
+	retract(clear(X)),
+	retract(clear(hand)).
 
-action(Ambient,pickup_b,Ambient2) :-
-	not(member(held(_), Ambient)),
-	not(member(on(_,b), Ambient)),
-        del( on(b,_), Ambient, Ambient1),
-	add(held(b), Ambient1, Ambient2).
-
-action(Ambient,pickup_c,Ambient2) :-
-	not(member(held(_), Ambient)),
-	not(member(on(_,c), Ambient)),
-        del( on(c,_), Ambient, Ambient1),
-	add(held(c), Ambient1, Ambient2).
-
-%Puts on Table
-action(Ambient,put_a_on_table, Ambient2) :-
-	member(held(a), Ambient),
-        del(held(a), Ambient, Ambient1),
-	add(on(a,m), Ambient1, Ambient2).
-
-action(Ambient,put_b_on_table, Ambient2) :-
-	member(held(b), Ambient),
-        del(held(b), Ambient, Ambient1),
-	add(on(b,m), Ambient1, Ambient2).
-
-action(Ambient,put_c_on_table, Ambient2) :-
-	member(held(c), Ambient),
-        del(held(c), Ambient, Ambient1),
-	add(on(c,m), Ambient1, Ambient2).
-
-%Putdowns
-action(Ambient,putdown_ab,Ambient2) :-
-	member(held(a), Ambient),
-        del(held(a), Ambient, Ambient1),
-	add(on(a,b), Ambient1, Ambient2).
-
-action(Ambient,putdown_ac,Ambient2) :-
-	member(held(a), Ambient),
-        del(held(a), Ambient, Ambient1),
-	add(on(a,c), Ambient1, Ambient2).
-
-action(Ambient,putdown_ba,Ambient2) :-
-	member(held(b), Ambient),
-        del(held(b), Ambient, Ambient1),
-	add(on(b,a), Ambient1, Ambient2).
-
-action(Ambient,putdown_bc,Ambient2) :-
-	member(held(b), Ambient),
-        del(held(b), Ambient, Ambient1),
-	add(on(b,c), Ambient1, Ambient2).
-
-action(Ambient,putdown_ca,Ambient2) :-
-	member(held(c), Ambient),
-        del(held(c), Ambient, Ambient1),
-	add(on(c,a), Ambient1, Ambient2).
-
-action(Ambient,putdown_cb,Ambient2) :-
-	member(held(c), Ambient),
-        del(held(c), Ambient, Ambient1),
-	add(on(c,b), Ambient1, Ambient2).
-
-
-
-%Solving
-
-solve(Ambient1, Ambient1).
-solve(Ambient1,Ambient2) :-
-	action(Ambient1, Move, Intermediary),
-	solve(Intermediary, Ambient2).
-
+putdown(X,A) :-
+      %precond
+	held(X),
+	clear(A),
+	A \== table,
+	A \== X,
+      %effects
+        assert(clear(hand)),
+	assert(on(X,A)),
+	assert(clear(X)),
+	retract(clear(A)),
+	retract(held(X)).
 
